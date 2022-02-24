b37d2dd83a7a21ae52e80bf4044941db6d73994f
Rohan Raj <rohanraj7316@gmail.com>
2022-02-24 15:26:33 +0530

    feat: updated the handling of response errors

---
+++ helpers/README.md
@@ -0,0 +1,5 @@
+# Helpers
+
+## Response
+
+it helps us to struct the responses in the proper manner
--- helpers/response.go
+++ helpers/response.go
@@ -10,6 +10,7 @@ import (
 type ErrorStruct struct {
 	Code    int    `json:"code,omitempty"`
 	Message string `json:"message,omitempty"`
+	Stack   string `json:"stack,omitempty"`
 }
 
 type ResponseBodyStruct struct {
@@ -20,20 +21,31 @@ type ResponseBodyStruct struct {
 	Data       interface{} `json:"data,omitempty"`
 }
 
-func ResponseBody(c *fiber.Ctx, statusCode int, message string, data interface{}, err error) error {
+// ResponseBody should be
+func ResponseBody(c *fiber.Ctx, statusCode int, message string, data interface{}, err interface{}) error {
 	rBody := ResponseBodyStruct{
 		StatusCode: statusCode,
 		Status:     http.StatusText(statusCode),
 		Message:    message,
 		Data:       data,
-		Err: ErrorStruct{
-			Message: err.Error(),
-		},
 	}
 
-	err = c.Status(statusCode).JSON(rBody)
+	switch t := err.(type) {
+	default:
+		rBody.Err.Message = t.(string)
+	case error:
+		rBody.Err.Message = t.Error()
+	case ErrorStruct:
+		rBody.Err.Code = t.Code
+		rBody.Err.Message = t.Message
+		rBody.Err.Stack = t.Stack
+	case string:
+		rBody.Err.Message = t
+	}
+
+	iErr := c.Status(statusCode).JSON(rBody)
 	if err != nil {
-		return err
+		return iErr
 	}
 	return nil
 }
