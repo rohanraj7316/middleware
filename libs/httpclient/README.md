7abbbeeb4bc04cddb5eea648c6f72f6bce992ef1
Rohan Raj <rohanraj7316@gmail.com>
2022-04-04 17:02:57 +0530

    docs: updaet httpclient docs

--- libs/httpclient/README.md
+++ libs/httpclient/README.md
@@ -1,1 +1,54 @@
 # Httpclient
+
+Wrapper build on [httpclient](https://github.com/rohanraj7316/httpclient)
+
+## Signature
+
+```
+func New(config ...httpclient.Config) (*HttpClient, error)
+```
+
+### Config
+
+```
+// https://github.com/rohanraj7316/httpclient config
+type Config struct {
+	// Timeout gives you timeout for request
+	// Default: 30s
+	Timeout time.Duration
+
+	// bool flag which help us in configuring proxy
+	// Default: false
+	UseProxy bool
+
+	// url need to do the proxy
+	// Default: nil
+	ProxyURL string
+
+	// LogReqResEnable helps in logging request & responses.
+	// Default true
+	LogReqResEnable bool
+
+	// LogReqResBodyEnable helps in logging request and responses body
+	// Default true
+	LogReqResBodyEnable bool
+}
+```
+
+### Request
+
+```
+// Request should be used for sending http request with in
+// resource model. helps to use fiber.Ctx insted of generating
+// onw of it's own.
+// Note: never create your own context.
+```
+
+### RequestSDK
+
+```
+// RequestSDK should be used to send http request.
+// trigger it from SDK. has an added layer for checking
+// requestId.
+// Note: never create your own context.
+```
--- libs/httpclient/httpclient.go
+++ libs/httpclient/httpclient.go
@@ -1,6 +1,8 @@
 package httpclient
 
 import (
+	"context"
+	"fmt"
 	"io"
 	"net/http"
 
@@ -26,10 +28,26 @@ func New(config ...httpclient.Config) (*HttpClient, error) {
 	}, nil
 }
 
-// Request responsible for sending http request
-// by using the Config set at the time of initialization.
+// Request should be used for sending http request with in
+// resource model. helps to use fiber.Ctx insted of generating
+// onw of it's own.
+// Note: never create your own context.
 func (hClient *HttpClient) Request(c *fiber.Ctx, method, url string, headers map[string]string,
 	request io.Reader) (*http.Response, error) {
 	rCtx := c.UserContext()
 	return hClient.client.Request(rCtx, method, url, headers, request)
+}
+
+// RequestSDK should be used to send http request.
+// trigger it from SDK. has an added layer for checking
+// requestId.
+// Note: never create your own context.
+func (hClient *HttpClient) RequestSDK(c context.Context, method, url string, headers map[string]string,
+	request io.Reader) (*http.Response, error) {
+	rID := c.Value("requestId")
+	if rID == "" {
+		return nil, fmt.Errorf("'requestId' is missing in the context")
+	}
+
+	return hClient.client.Request(c, method, url, headers, request)
 }
