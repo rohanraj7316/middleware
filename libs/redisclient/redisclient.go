eb0320355a6dc78730b4adccc69f83c36afb8fc1
Rohan Raj <rohanraj7316@gmail.com>
2022-04-21 15:44:56 +0530

    docs: update comment

--- libs/redisclient/redisclient.go
+++ libs/redisclient/redisclient.go
@@ -4,6 +4,7 @@ import (
 	"github.com/go-redis/redis/v8"
 )
 
+// New responsible for creating new redis client
 func New(config ...Config) (Config, error) {
 	cfg, err := configDefault(config...)
 	if err != nil {
