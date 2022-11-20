c0a57d156f7ceb901c459f309e23bdf8f23690a8
Rohan Raj <rohanraj7316@gmail.com>
2022-11-20 13:02:45 +0530

    feat: update config

--- configs/configs.go
+++ configs/configs.go
@@ -5,6 +5,7 @@ import "os"
 // GetValue retrieves the value of environment variable named by the key.
 // It returns the value, if not then it will return the default value passed
 // as the second parameter.
+// Depricated: this function is been removed from configs and moved into lib/env
 var GetValue = func(k, d string) string {
 	if v := os.Getenv(k); v != "" {
 		return v
